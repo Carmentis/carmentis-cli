6ba02a92b36f2a31678cd09526b718a2dababffb
Gaël Marcadet <gamarcadet@gmail.com>
2026-01-28 18:21:33 +0100

    chore: delete dist

--- dist/node/commands/crypto/gen/SeedGenCommand.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SeedGenCommand = void 0;
-class SeedGenCommand {
-    static register(program) {
-        const seedGenCommand = program;
-    }
-}
-exports.SeedGenCommand = SeedGenCommand;
